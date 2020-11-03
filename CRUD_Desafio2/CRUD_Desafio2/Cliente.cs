58c2533f3f2ee16f5da65c912d1a3cbe24776892
Douglas Tertuliano <douglasjtds@gmail.com>
2020-11-02 22:25:57 -0300

    first steps

---
+++ CRUD_Desafio2/CRUD_Desafio2/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD_Desafio2
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int CPF { get; set; }
+        public string Email { get; set; }
+        public int Telefone { get; set; }
+        public string Endereco { get; set; }
+    }
+}
--- CRUD_Desafio2/CRUD_Desafio2/Program.cs
+++ CRUD_Desafio2/CRUD_Desafio2/Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace CRUD_Desafio2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Exercício de CRUD do Desafio 2! TRILHA BACKEND!\r");
+            Console.WriteLine("------------------------\n");
+
+
+            // Ask the user to choose an option.
+            Console.WriteLine("Escolha a opção que deseja executar:");
+            Console.WriteLine("\tc - Create");
+            Console.WriteLine("\tr - Read");
+            Console.WriteLine("\tu - Update");
+            Console.WriteLine("\td - Delete");
+            Console.Write("O que deseja executar? ");
+
+            // Use a switch statement to call the methods.
+            switch (Console.ReadLine())
+            {
+                case "c":
+                    Create();
+                    break;
+                case "r":
+                    Read();
+                    break;
+                case "u":
+                    Update();
+                    break;
+                case "d":
+                    Delete();
+                    break;
+            }
+            // Wait for the user to respond before closing.
+            Console.Write("Press any key to close the CRUD console app...");
+            Console.ReadKey();
+
+
+
+            #region [CRUD methods]
+            void Create()
+            {
+
+            }
+
+            void Read()
+            {
+
+            }
+
+            void Update()
+            {
+
+            }
+
+            void Delete()
+            {
+
+            } 
+            #endregion
         }
     }
 }
