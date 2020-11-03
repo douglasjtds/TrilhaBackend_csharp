22a52785ab20b1a8a380ca40524e9f54cbc017a5
Douglas Tertuliano <douglasjtds@gmail.com>
2020-11-02 22:04:44 -0300

    create project

---
+++ CRUD_Desafio2/CRUD_Desafio2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUD_Desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
