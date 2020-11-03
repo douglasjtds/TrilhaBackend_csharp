4457da02845a723be351c404b82b16f3d9408ccf
Douglas Tertuliano <douglasjtds@gmail.com>
2020-11-02 23:20:46 -0300

    method create

--- CRUD_Desafio2/CRUD_Desafio2/Cliente.cs
+++ CRUD_Desafio2/CRUD_Desafio2/Cliente.cs
@@ -7,10 +7,10 @@ namespace CRUD_Desafio2
     public class Cliente
     {
         public string Nome { get; set; }
-        public int Idade { get; set; }
-        public int CPF { get; set; }
+        public string Idade { get; set; }
+        public string CPF { get; set; }
         public string Email { get; set; }
-        public int Telefone { get; set; }
+        public string Telefone { get; set; }
         public string Endereco { get; set; }
     }
 }
--- CRUD_Desafio2/CRUD_Desafio2/Program.cs
+++ CRUD_Desafio2/CRUD_Desafio2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
 
 namespace CRUD_Desafio2
@@ -44,7 +45,28 @@ namespace CRUD_Desafio2
             #region [CRUD methods]
             void Create()
             {
+                //List<Cliente> clientes = new List<Cliente>();
+                Cliente cliente = new Cliente();
 
+                //nome, idade, CPF, e-mail, telefone e endereço
+                Console.WriteLine("\nVocê escolheu adicionar um novo cliente.\n");
+                Console.Write("Digite o nome do novo cliente: ");
+                cliente.Nome = Console.ReadLine();
+
+                Console.Write("Digite a idade do novo cliente: ");
+                cliente.Idade = Console.ReadLine();
+
+                Console.Write("Digite o CPF do novo cliente: ");
+                cliente.CPF = Console.ReadLine();
+
+                Console.Write("Digite o e-mail do novo cliente: ");
+                cliente.Email = Console.ReadLine();
+
+                Console.Write("Digite o telefone do novo cliente: ");
+                cliente.Telefone = Console.ReadLine();
+
+                Console.Write("Digite o endereço do novo cliente: ");
+                cliente.Endereco = Console.ReadLine();
             }
 
             void Read()
