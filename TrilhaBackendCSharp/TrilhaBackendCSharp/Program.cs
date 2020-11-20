a8efe74beafa3948ab560a2c78c4702a6ba29ad7
Douglas Tertuliano <douglasjtds@gmail.com>
2020-11-19 22:01:22 -0300

    correção das chamadas dos métodos no Program.cs

--- TrilhaBackendCSharp/TrilhaBackendCSharp/Program.cs
+++ TrilhaBackendCSharp/TrilhaBackendCSharp/Program.cs
@@ -13,7 +13,7 @@ namespace TrilhaBackendCSharp
         {
             var repository = new ClienteRepositorio();
 
-            //teste consultar
+            /*//teste consultar
             foreach (var cliente in repository.Consultar(""))
                 Console.WriteLine(cliente.Imprimir);
 
@@ -33,13 +33,13 @@ namespace TrilhaBackendCSharp
             Cliente atualizarCliente = new Cliente("Douglas", 25, "123456789", "douglas.tertuliano@zup.com.br", "91848090", "Rua São Geraldo");
             repository.Salvar(atualizarCliente);
             foreach (var cliente in repository.Consultar())
-                Console.WriteLine(cliente.Imprimir);
+                Console.WriteLine(cliente.Imprimir); */
 
-            /*Console.WriteLine("Exercício de CRUD do Desafio 2! TRILHA BACKEND!\r");
+            Console.WriteLine("Exercício de CRUD do Desafio 2! TRILHA BACKEND!\r");
             Console.WriteLine("------------------------\n");
 
-            List<Cliente> clientes = new List<Cliente>();
-            Cliente cliente = new Cliente();
+            //List<Cliente> clientes = new List<Cliente>();
+            //Cliente cliente = new Cliente();
 
             int menuControl;
 
@@ -87,24 +87,26 @@ namespace TrilhaBackendCSharp
                 //A operação de Create deve popular o cadastro de um cliente e inserir na estrutura.
                 Console.WriteLine("\nVocê escolheu adicionar um novo cliente.\n");
                 Console.Write("Digite o nome do novo cliente: ");
-                cliente.Nome = Console.ReadLine();
+                var nome = Console.ReadLine();
 
                 Console.Write("Digite a idade do novo cliente: ");
-                cliente.Idade = Console.ReadLine();
+                int idade = Convert.ToInt32(Console.ReadLine());
 
                 Console.Write("Digite o CPF do novo cliente: ");
-                cliente.CPF = Console.ReadLine();
+                var cpf = Console.ReadLine();
 
                 Console.Write("Digite o e-mail do novo cliente: ");
-                cliente.Email = Console.ReadLine();
+                var email = Console.ReadLine();
 
                 Console.Write("Digite o telefone do novo cliente: ");
-                cliente.Telefone = Console.ReadLine();
+                var telefone = Console.ReadLine();
 
                 Console.Write("Digite o endereço do novo cliente: ");
-                cliente.Endereco = Console.ReadLine();
+                var endereco = Console.ReadLine();
 
-                clientes.Add(cliente);
+                var cliente = new Cliente(nome, idade, cpf, email, telefone, endereco);
+
+                repository.Salvar(cliente);
 
                 Console.WriteLine("\nNovo cliente cadastrado com sucesso.\n");
 
@@ -115,47 +117,13 @@ namespace TrilhaBackendCSharp
             {
                 //A operação de Read  deve permitir visualizar todos os clientes ou um só pelo CPF.
                 Console.WriteLine("\nVocê escolheu a opção de buscar por cliente(s).\n");
-                Console.WriteLine("Deseja buscar por todos os clientes ou por um específico? ");
+                Console.WriteLine("Você pode digitar o CPF específico para buscar ou apenas apertar enter para buscar todos: ");
 
-                Console.WriteLine("\tt - Todos");
-                Console.WriteLine("\te - Um específico");
+                var valorDigitado = Console.ReadLine();
 
-                switch (Console.ReadLine())
-                {
-                    case "t":
-                        foreach (var item in clientes)
-                        {
-                            Console.WriteLine("Nome: {0}", item.Nome);
-                            Console.WriteLine("Idade: {0}", item.Idade);
-                            Console.WriteLine("CPF: {0}", item.CPF);
-                            Console.WriteLine("Email: {0}", item.Email);
-                            Console.WriteLine("Telefone: {0}", item.Telefone);
-                            Console.WriteLine("Endereco: {0}", item.Endereco);
-                        }
-
-                        Console.WriteLine("Pressione Enter para continuar...");
-                        Console.ReadKey();
-                        break;
-                    case "e":
-                        Console.WriteLine("Digite o CPF do cliente que deseja visualizar: ");
-                        var cpfBuscado = Console.ReadLine();
-
-                        foreach (var item in clientes)
-                        {
-                            if (item.CPF == cpfBuscado)
-                            {
-                                Console.WriteLine("Nome: {0}", item.Nome);
-                                Console.WriteLine("Idade: {0}", item.Idade);
-                                Console.WriteLine("CPF: {0}", item.CPF);
-                                Console.WriteLine("Email: {0}", item.Email);
-                                Console.WriteLine("Telefone: {0}", item.Telefone);
-                                Console.WriteLine("Endereco: {0}", item.Endereco);
-                            }
-                        }
-                        Console.WriteLine("Pressione Enter para continuar...");
-                        Console.ReadKey();
-                        break;
-                }
+                //teste consultar
+                foreach (var cliente in repository.Consultar(valorDigitado))
+                    Console.WriteLine("\n" + cliente.Imprimir);
 
                 Console.Clear();
             }
@@ -165,29 +133,30 @@ namespace TrilhaBackendCSharp
                 //A operação de Update deve permitir alterar um ou mais dos dados de um cliente a partir do CPF dele(O campo CPF não pode ser alterado).
                 Console.WriteLine("\nVocê escolheu a opção de atualizar os dados de um cliente.\n");
                 Console.WriteLine("Digite o CPF do cliente que deseja atualizar (O campo CPF não pode ser alterado): ");
-
                 var cpfCliente = Console.ReadLine();
-                foreach (var item in clientes)
-                {
-                    if (cpfCliente == item.CPF)
-                    {
-                        Console.Write("Digite o novo nome do novo cliente: ");
-                        cliente.Nome = Console.ReadLine();
 
-                        Console.Write("Digite a nova idade do novo cliente: ");
-                        cliente.Idade = Console.ReadLine();
+                Console.WriteLine("\nAdicione os novos dados desse Cliente.\n");
 
-                        Console.Write("Digite o novo e-mail do novo cliente: ");
-                        cliente.Email = Console.ReadLine();
+                Console.Write("Digite o novo nome do cliente: ");
+                var nome = Console.ReadLine();
 
-                        Console.Write("Digite o novo telefone do novo cliente: ");
-                        cliente.Telefone = Console.ReadLine();
+                Console.Write("Digite a nova idade do cliente: ");
+                var idade = Convert.ToInt32(Console.ReadLine());
 
-                        Console.Write("Digite o novo endereço do novo cliente: ");
-                        cliente.Endereco = Console.ReadLine();
-                    }
-                    clientes.Add(item); //acho que não posso fazer isso pq vai ficar dois clientes com o mesmo CPF e dados diferentes
-                }
+                Console.Write("Digite o novo e-mail do cliente: ");
+                var email = Console.ReadLine();
+
+                Console.Write("Digite o novo telefone do cliente: ");
+                var telefone = Console.ReadLine();
+
+                Console.Write("Digite o novo endereco do cliente: ");
+                var endereco = Console.ReadLine();
+
+                var clienteAtualizado = new Cliente(nome, idade, cpfCliente, email, telefone, endereco);
+                repository.Salvar(clienteAtualizado);
+
+                Console.WriteLine("\nCliente atualizado com sucesso.\n");
+
                 Console.Clear();
             }
 
@@ -198,11 +167,10 @@ namespace TrilhaBackendCSharp
                 Console.WriteLine("Digite o CPF do cliente que deseja excluir: ");
                 var cpfCliente = Console.ReadLine();
 
-                foreach (var item in clientes)
-                {
-                    if (cpfCliente == item.CPF)
-                        clientes.Remove(item);
-                }
+                if (repository.Remover(cpfCliente))
+                    Console.WriteLine("\nCliente com o CPF {0} removido com sucesso.\n", cpfCliente);
+                else
+                    Console.WriteLine("\nCliente com o CPF {0} não encontrado na base.\n", cpfCliente);
 
                 Console.Clear();
             }
@@ -215,7 +183,7 @@ namespace TrilhaBackendCSharp
                 Console.WriteLine("Você saiu do programa. Clique qualquer tecla para fechar...");
             }
             #endregion
-            */
+
         }
     }
 }
